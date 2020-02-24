1a5c909fcb184cdef3cadb61ea5df1eb565af6f8
DesertCamel <35378010+MidoCobra@users.noreply.github.com>
2020-02-24 09:06:25 +0200

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -24,4 +24,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',jobs.views.home,name='home'),
     path('blog/',include('blog.urls')),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
+
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
